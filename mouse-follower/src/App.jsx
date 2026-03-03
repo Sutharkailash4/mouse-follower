d136b5dfb3ee889150639a1f0b10289eaa79170b
Kailash Sutahr <kailashsuthar4488@gmail.com>
2026-03-03 17:55:16 +0530

    add outer circle

--- mouse-follower/src/App.jsx
+++ mouse-follower/src/App.jsx
@@ -1,15 +1,16 @@
 import React, { useState } from "react";
 
 const App = () => {
-  const [leftVal,setLeftVal] = useState(200);
+  const [leftVal,setLeftVal] = useState(0);
   const [rightVal,setRightVal] = useState(0);
+  // const [leftValouter,setRightValouter] = useState(0)
   return (
     <div className="app" onMouseMove={(e)=>{
       setLeftVal(e.clientY);
       setRightVal(e.clientX);
     }}>
-      <div className="circle_outer">
-      <div style={{position:"absolute" ,top:`${leftVal}px` ,left:`${rightVal}px`}} className="circle">
+      <div className="circle_outer" style={{position:"absolute" , top:`${leftVal}px`, left:`${rightVal}px`}}>
+      <div className="circle">
         
       </div>
       </div>
